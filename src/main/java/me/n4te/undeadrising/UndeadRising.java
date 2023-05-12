cb815412ef49729c2477a20defede93b1b3356f1
n4te <xtrememoss@gmail.com>
2023-05-12 20:05:31 +0800

    Testing

--- src/main/java/me/n4te/undeadrising/UndeadRising.java
+++ src/main/java/me/n4te/undeadrising/UndeadRising.java
@@ -1,17 +1,22 @@
 package me.n4te.undeadrising;
 
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.entity.Zombie;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import java.util.Random;
 
-public final class UndeadRising extends JavaPlugin {
-
-    @Override
-    public void onEnable() {
-        // Plugin startup logic
-
-    }
-
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+public class UndeadRising implements Listener {
+    @EventHandler
+    public void onCreatureSpawn(CreatureSpawnEvent event) {
+        if (event.getEntity() instanceof Zombie) {
+            Random rand = new Random();
+            int chance = rand.nextInt(100);
+            if (chance < 99) {
+                Zombie zombie = (Zombie) event.getEntity();
+                zombie.setCustomName("Infected");
+                zombie.setCustomNameVisible(true);
+            }
+        }
     }
 }
